51ee5bb3dfe5d34bc5ecc10b207b7c5305646a07
afkguy <afkguy@gmail.com>
2025-07-03 09:31:34 +0200

    Update Botmon.lua

--- Botmon.lua
+++ Botmon.lua
@@ -205,26 +205,6 @@ end
 -- 🔹 FileIslandQuest Functions Section
 ---------------------------------------------
 local FileIslandQuest = {}
-function FileIslandQuest.CheckProgress()
-	local allCompleted = true
-	local quests = { 1032, 1033, 1035 }
-	for _, questId in ipairs(quests) do
-		if not BotHelper.IsQuestComplete(questId) then
-			allCompleted = false
-			if UtilityHelper.WaitForMap() then
-				UtilityHelper.SafeCall(BotHelper.AutoQuestAddQuest, questId)
-			end
-		end
-	end
-	if UtilityHelper.WaitForMap() then
-		UtilityHelper.SafeCall(BotHelper.AutoQuestToggle, true)
-	end
-	if not allCompleted then
-		_FileIslandHandleQuest()
-	end
-	return allCompleted
-end
-
 local function _FileIslandHandleQuest()
 	local quests = {
 		[7015] = function()
@@ -260,10 +240,45 @@ local function _FileIslandHandleQuest()
 	end
 end
 
+function FileIslandQuest.CheckProgress()
+	local allCompleted = true
+	local quests = { 1032, 1033, 1035 }
+	for _, questId in ipairs(quests) do
+		if not BotHelper.IsQuestComplete(questId) then
+			allCompleted = false
+			if UtilityHelper.WaitForMap() then
+				UtilityHelper.SafeCall(BotHelper.AutoQuestAddQuest, questId)
+			end
+		end
+	end
+	if UtilityHelper.WaitForMap() then
+		UtilityHelper.SafeCall(BotHelper.AutoQuestToggle, true)
+	end
+	if not allCompleted then
+		_FileIslandHandleQuest()
+	end
+	return allCompleted
+end
+
 ---------------------------------------------
 -- 🔹 FileIslandQuest Functions Section
 ---------------------------------------------
 local ServerContinentQuest = {}
+local function _ServerContinentHandleQuest()
+	local quests = {
+		[70150] = function()
+			LogHelper.LogMessage("[SERVER CONTINENT] Assisting with Quest 7015: Wake Up, Leomon!")
+			QuestHelper.SummonBoss(154003, 99100, true)
+			Sleep(5)
+		end,
+	}
+	for questId, handler in pairs(quests) do
+		if BotHelper.IsQuestOnGoing(questId) then
+			handler()
+		end
+	end
+end
+
 function ServerContinentQuest.CheckProgress()
 	local allCompleted = true
 	local quests = { 1071, 1265, 1266 }
@@ -284,47 +299,12 @@ function ServerContinentQuest.CheckProgress()
 	return allCompleted
 end
 
-local function _ServerContinentHandleQuest()
-	local quests = {
-		[70150] = function()
-			LogHelper.LogMessage("[SERVER CONTINENT] Assisting with Quest 7015: Wake Up, Leomon!")
-			QuestHelper.SummonBoss(154003, 99100, true)
-			Sleep(5)
-		end,
-	}
-	for questId, handler in pairs(quests) do
-		if BotHelper.IsQuestOnGoing(questId) then
-			handler()
-		end
-	end
-end
-
 
 ---------------------------------------------
 -- 🔹 EventQuest Functions Section
 ---------------------------------------------
 local EventQuest = {}
-function EventQuest.CheckProgress()
-	local allCompleted = true
-	local quests = { 6280, 6686 }
-	for _, questId in ipairs(quests) do
-		if not BotHelper.IsQuestComplete(questId) then
-			allCompleted = false
-			if UtilityHelper.WaitForMap() then
-				UtilityHelper.SafeCall(BotHelper.AutoQuestAddQuest, questId)
-			end
-		end
-	end
-	if UtilityHelper.WaitForMap() then
-		UtilityHelper.SafeCall(BotHelper.AutoQuestToggle, true)
-	end
-	if not allCompleted then
-		_HandleQuest()
-	end
-	return allCompleted
-end
-
-local function _HandleQuest()
+local function _EventHandleQuest()
 	local quests = {
 		[6683] = function()
 			LogHelper.LogMessage("[EVENT] Assisting with Quest 6683: GateKeeper of Balance!")
@@ -340,6 +320,26 @@ local function _HandleQuest()
 			handler()
 		end
 	end
+end
+
+function EventQuest.CheckProgress()
+	local allCompleted = true
+	local quests = { 6280, 6686 }
+	for _, questId in ipairs(quests) do
+		if not BotHelper.IsQuestComplete(questId) then
+			allCompleted = false
+			if UtilityHelper.WaitForMap() then
+				UtilityHelper.SafeCall(BotHelper.AutoQuestAddQuest, questId)
+			end
+		end
+	end
+	if UtilityHelper.WaitForMap() then
+		UtilityHelper.SafeCall(BotHelper.AutoQuestToggle, true)
+	end
+	if not allCompleted then
+		_EventHandleQuest()
+	end
+	return allCompleted
 end
 
 ---------------------------------------------
